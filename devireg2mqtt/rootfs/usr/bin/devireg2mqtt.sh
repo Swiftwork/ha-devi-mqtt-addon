cc3b9a117b5b21a9d6d105288161c91543fdefe5
Erik Hughes <erik.hughes@outlook.com>
2024-10-06 14:58:07 +0200

    feat: updated locations

--- devireg2mqtt/rootfs/usr/bin/devireg2mqtt.sh
+++ devireg2mqtt/rootfs/usr/bin/devireg2mqtt.sh
@@ -5,7 +5,7 @@ jq '.devi' /data/options.json > /app/config/devi_config.json
 jq '.mqtt' /data/options.json > /app/config/mqtt_config.json
 
 # Run the application
-java -cp ha-devi-mqtt.jar io.homeassistant.devi.mqtt.service.ConsoleRunner \
+java -cp /app/ha-devi-mqtt.jar io.homeassistant.devi.mqtt.service.ConsoleRunner \
   --auto-discovery-templates /app/config/auto-discovery-templates \
   --mqtt-config /app/config/mqtt_config.json \
   --devi-config /app/config/devi_config.json
